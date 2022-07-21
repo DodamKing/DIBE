81920827ffd47289a2eabdbd93579004be242768
dodamking <power6120@naver.com>
2022-07-21 13:29:44 +0900

    컬랙션 생성 및 파일 저장 테스트

---
+++ dive/models/index.js
@@ -0,0 +1,7 @@
+const Song = require('./Song')
+
+const db = {}
+
+db.Song = Song
+
+module.exports = db
--- dive/routes/song.js
+++ dive/routes/song.js
@@ -1,8 +1,67 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../models')
 
-router.get('/upload', (req, res) => {
-    res.json('업로드 만들꺼임')
+router.get('/list', async (req, res) => {
+    try {
+        const result = await db.Song.find()
+        res.json(result)
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+router.get('/:_id', async (req, res) => {
+    try {
+        const _id = req.params._id
+        const result = await db.Song.findById(_id)
+        console.log(result.title)
+        // console.log(result.file)
+
+        const Readable = require('stream').Readable
+        let stream = new Readable()
+        stream.push(result.file)
+        stream.push(null)
+        console.log(stream);
+
+        res.json(stream)
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+router.post('/upload', async (req, res) => {
+    const data = req.body
+    try {
+        await db.Song.create(data)
+        res.json('upsert success!')
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+router.post('/upload/test', (req, res) => {
+    const data = req.body
+    try {
+        const fs = require('fs')
+        fs.readFile('d:/dd/etc/DIVE/music_db/보고싶었어 - WSG워너비 (4FIRE).mp3', (err, file) => {
+            data.file = file
+            db.Song.create(data)
+            res.json(result)
+        })
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+router.delete('/delete', async  (req, res) => {
+    try {
+        const _id = req.query._id
+        const result = await db.Song.findByIdAndDelete(_id)
+        res.json(result)
+    } catch (err) {
+        console.error(err)
+    }
 })
 
 module.exports = router
