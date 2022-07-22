18c08165d74766f92785aad2bb30d1a7aa73da28
dodamking <power6120@naver.com>
2022-07-22 18:43:14 +0900

    이진파일 재생법을 모름

--- dive/routes/song.js
+++ dive/routes/song.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const db = require('../models')
+const fs = require('fs')
 
 router.get('/list', async (req, res) => {
     try {
@@ -15,7 +16,8 @@ router.get('/:_id', async (req, res) => {
     try {
         const _id = req.params._id
         const result = await db.Song.findById(_id)
-        res.json(result)
+        console.log(result.file);
+        // res.json(result.file)
 
         // const Readable = require('stream').Readable
         // let stream = new Readable()
@@ -39,26 +41,44 @@ router.post('/upload', async (req, res) => {
 
 router.post('/upload/test', (req, res) => {
     const data = req.body
-    const title = 'BEAUTIFUL MONSTER'
-    const artist = 'STAYC(스테이씨)'
+    const title = '보고싶었어'
+    const artist = 'WSG워너비 (4FIRE)'
     const fileName = title + ' - ' + artist + '.mp3'
+    const path = 'e:/music_db/' + fileName
     data.title = title
     data.artist = artist
-    try {
-        const fs = require('fs')
-        fs.readFile('e:/music_db/' + fileName, (err, file) => {
-            if (err) {
-                console.error(err)
-            }
-            else {
-                data.file = file
-                db.Song.create(data)
-                res.json('succes!')
-            }
+    
+    fs.readFile(path, (err, file) => {
+        if (err) return res.json(err)
+        
+        data.file = file
+        db.Song.create(data, (err, result) => {
+            if (err) return res.json(err)
+            res.json(result)
         })
-    } catch (err) {
-        console.error(err)
-    }
+})
+    
+    // const readStream = fs.createReadStream(path)
+    // const stream = []
+
+    // readStream.on('data', (chunck) => {
+        //     stream.push(chunck)
+        // })
+
+    // readStream.on('end', async () => {
+    //     const file = Buffer.concat(stream)
+    //     data.file = file
+    //     try {
+    //         const result = await db.Song.create(data)
+    //         res.json(result)
+    //     } catch (err) {
+    //         console.error(err)
+    //     }
+    // })
+
+    // readStream.on('error', (err) => {
+    //     console.error(err)
+    // })
 })
 
 router.delete('/delete/:_id', async  (req, res) => {
