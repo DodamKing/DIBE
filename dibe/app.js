f7f5653e9e8de153d00f5d4286791da214c9b527
dodamking <power6120@naver.com>
2022-08-02 18:37:19 +0900

    passport를 사용하서 회원가입, 로그인 해보려 함

--- dibe/app.js
+++ dibe/app.js
@@ -3,6 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const session = require('express-session')
 const flash = require('connect-flash')
 
 var indexRouter = require('./routes/index');
@@ -21,10 +24,17 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  secret : 'key',
+  resave : false,
+  saveUninitialized : true,
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/e', express.static(path.join('')))
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/songs', songRouter)
--- dibe/routes/users.js
+++ dibe/routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const db = require('../models')
-const bcrypt = require('bcryptjs')
+// const bcrypt = require('bcryptjs')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
